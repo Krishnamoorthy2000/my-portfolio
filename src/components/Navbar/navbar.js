4c4d709bcf4cd9aa99fd9a4df1b58f77ab5fa929
krishnamoorthy2000 <krishnamalathi2000@gmail.com>
2023-05-01 07:34:27 +0530

    s

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Button, Container, Nav, Navbar } from 'react-bootstrap';
-import { ImHome ,ImUserTie,ImBubbles,ImQuill,ImBooks} from 'react-icons/im';
-import './navbar.css'; // import CSS file here
+import React from "react";
+import { Link } from "react-router-dom";
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
+import { ImHome, ImUserTie, ImBubbles, ImQuill, ImBooks } from "react-icons/im";
+import "./navbar.css"; // import CSS file here
 
 export default function NavbarContent() {
   function downloadResume() {
@@ -15,15 +15,16 @@ export default function NavbarContent() {
     link.click();
     document.body.removeChild(link);
   }
-  
+
   return (
     <Navbar
-
-    // expanded={expand}
-    fixed="top"
-    expand="md"
-     className="navbar" 
-     > {/* add inline style to set background color */}
+      // expanded={expand}
+      fixed="top"
+      expand="md"
+      className="navbar"
+    >
+      {" "}
+      {/* add inline style to set background color */}
       <Container fluid className="container">
         <Navbar.Brand as={Link} to="/" className="navbar-brand">
           Portfolio
@@ -32,22 +33,33 @@ export default function NavbarContent() {
         <Navbar.Collapse id="navbar-nav">
           <Nav className="ms-auto items align-self-*-center">
             <Nav.Link as={Link} to="/" className="nav-link">
-              <ImHome className="navbar-icons" />Home
+              <ImHome className="navbar-icons" />
+              Home
             </Nav.Link>
             <Nav.Link as={Link} to="/About" className="nav-link">
-              <ImUserTie className="navbar-icons" />About
+              <ImUserTie className="navbar-icons" />
+              About
             </Nav.Link>
             <Nav.Link as={Link} to="/Skill" className="nav-link">
-              <ImQuill className="navbar-icons" />Skill
+              <ImQuill className="navbar-icons" />
+              Skill
             </Nav.Link>
             <Nav.Link as={Link} to="/Project" className="nav-link">
-              <ImBooks className="navbar-icons" />Project
+              <ImBooks className="navbar-icons" />
+              Project
             </Nav.Link>
             <Nav.Link as={Link} to="/Contact" className="nav-link">
-              <ImBubbles className="navbar-icons" />Contact
+              <ImBubbles className="navbar-icons" />
+              Contact
             </Nav.Link>
-            <Button  className='resume' onClick={downloadResume}>resume</Button>
-
+            <Button className="resume">
+              <a
+                href="https://drive.google.com/file/d/10jVn0t_fLBLx3XdDCvgbCqmwdM9SrS6x/view?usp=sharing"
+                target="_blank"
+              >
+                Resume
+              </a>
+            </Button>
           </Nav>
         </Navbar.Collapse>
       </Container>
